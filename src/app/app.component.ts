d7cc271bb8a4d6b19fcd10f3f3d6dd598ac263da
mohamed.benfriha@epitech.eu <mohamed.benfriha@epitech.eu@epitech.eu>
2019-11-18 22:13:11 +0100

    update cookie

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,7 @@ import {ToastrService} from 'ngx-toastr';
 import  { LoaderService } from "./core/service/loader.service";
 
 import {
-    NgcCookieConsentService,
-    NgcInitializeEvent,
-    NgcNoCookieLawEvent,
-    NgcStatusChangeEvent
+    NgcCookieConsentService
 } from 'ngx-cookieconsent';
 
 import { DOCUMENT } from '@angular/common';
@@ -83,36 +80,7 @@ export class AppComponent implements OnInit, OnDestroy {
             }
         });
 
-        // subscribe to cookieconsent observables to react to main events
-        this.popupOpenSubscription = this.ccService.popupOpen$.subscribe(
-            () => {
-                // you can use this.ccService.getConfig() to do stuff...
-            });
 
-        this.popupCloseSubscription = this.ccService.popupClose$.subscribe(
-            () => {
-                // you can use this.ccService.getConfig() to do stuff...
-            });
-
-        this.initializeSubscription = this.ccService.initialize$.subscribe(
-            (event: NgcInitializeEvent) => {
-                // you can use this.ccService.getConfig() to do stuff...
-            });
-
-        this.statusChangeSubscription = this.ccService.statusChange$.subscribe(
-            (event: NgcStatusChangeEvent) => {
-                // you can use this.ccService.getConfig() to do stuff...
-            });
-
-        this.revokeChoiceSubscription = this.ccService.revokeChoice$.subscribe(
-            () => {
-                // you can use this.ccService.getConfig() to do stuff...
-            });
-
-        this.noCookieLawSubscription = this.ccService.noCookieLaw$.subscribe(
-            (event: NgcNoCookieLawEvent) => {
-                // you can use this.ccService.getConfig() to do stuff...
-            });
 
         this.translateService//
             .get(['cookie.header', 'cookie.message', 'cookie.dismiss', 'cookie.allow', 'cookie.deny', 'cookie.link', 'cookie.policy'])
@@ -146,11 +114,6 @@ export class AppComponent implements OnInit, OnDestroy {
 
     ngOnDestroy() {
         // unsubscribe to cookieconsent observables to prevent memory leaks
-        this.popupOpenSubscription.unsubscribe();
-        this.popupCloseSubscription.unsubscribe();
-        this.initializeSubscription.unsubscribe();
-        this.statusChangeSubscription.unsubscribe();
-        this.revokeChoiceSubscription.unsubscribe();
-        this.noCookieLawSubscription.unsubscribe();
+
     }
 }
