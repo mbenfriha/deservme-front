957c08f4207e33a656e1137dd0cc8b44ca6a0379
mohamed.benfriha@epitech.eu <mohamed.benfriha@epitech.eu@epitech.eu>
2019-11-25 18:50:04 +0100

    update url api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    baseUrl: 'http://localhost:3000/',
+    baseUrl: 'https://api.myquizzy.com/',
     shortUrl: 'https://qzzy.in/',
     admin: 'https://admin.myquizzy.com/',
     cookieDomain: '.myquizzy.com'
